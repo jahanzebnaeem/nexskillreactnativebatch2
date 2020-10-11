1bfe45a20b218d90a2d4e39c9a87550f605a41a8
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-10-11 17:50:50 +0500

    Lecture 3
    Working on header of albums app.

--- albums/index.js
+++ albums/index.js
@@ -2,8 +2,21 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+//
+// AppRegistry.registerComponent(appName, () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+// import dependencies
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import Header from './src/components/Header';
+
+// Create a component
+const App = () => {
+  return <Header />;
+};
+
+// Display the component
+AppRegistry.registerComponent('albums', () => App);
---
+++ albums/src/components/Header.js
@@ -0,0 +1,35 @@
+// import
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// Component
+const Header = () => {
+  const {viewStyle, textStyle} = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Album</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 30,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 0.2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  textStyle: {
+    fontSize: 18,
+  },
+};
+
+// Export
+export default Header;
