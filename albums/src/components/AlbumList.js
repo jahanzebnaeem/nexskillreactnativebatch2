d059a31b3f349c0a4288359f52a037f53670a07e
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-10-18 17:02:17 +0500

    Lecture 5
    Completed albums app.

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {ScrollView} from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -24,7 +24,7 @@ class AlbumList extends Component {
 
   render() {
     console.log(this.state);
-    return <View>{this.renderAlbums()}</View>;
+    return <ScrollView>{this.renderAlbums()}</ScrollView>;
   }
 }
 
