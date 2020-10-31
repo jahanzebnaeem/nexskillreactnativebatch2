b729ffa0ded40a8bc8bb5c746d12f48f19849dbb
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-10-31 15:41:21 +0500

    Lecture 8
    Redux and introduction to redux.

---
+++ tech_stack/src/components/common/Header.js
@@ -0,0 +1,35 @@
+// import
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// Component
+const Header = (props) => {
+  const {viewStyle, textStyle} = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 30,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 0.2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  textStyle: {
+    fontSize: 18,
+  },
+};
+
+// Export
+export {Header};
