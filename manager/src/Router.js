36cd3d0757b094dc320ed12c14a3b8b9ed3d5adf
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-11-26 18:35:05 +0500

    Lecture 13
    Add new libraries for navigation and start updating code for app navigation.

---
+++ manager/src/Router.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Router, Stack, Scene, Actions} from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Stack key="root">
+        <Scene key="login" component={LoginForm} title="Please Login" />
+        <Scene
+          onRight={() => Actions.employeeCreate()}
+          rightTitle="Add"
+          key="employeeList"
+          component={EmployeeList}
+          title="Employees"
+          left={() => null}
+        />
+        <Scene
+          key="employeeCreate"
+          component={EmployeeCreate}
+          title="Create Employee"
+          renderBackButton={() => null}
+        />
+      </Stack>
+    </Router>
+  );
+};
+
+export default RouterComponent;
--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import {Actions} from 'react-native-router-flux';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -47,4 +48,6 @@ const loginUserFail = (dispatch) => {
 
 const loginUserSuccess = (dispatch, user) => {
   dispatch({type: LOGIN_USER_SUCCESS, payload: user});
+
+  Actions.employeeList();
 };
