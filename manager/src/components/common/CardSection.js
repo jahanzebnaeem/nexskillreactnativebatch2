85ac212d01c8e3e3dac5517b13d31492cc162fc7
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-11-27 13:07:26 +0500

    Lecture 14
    Work on Employee Create form and added picker library.

--- manager/src/components/common/CardSection.js
+++ manager/src/components/common/CardSection.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {View} from 'react-native';
 
 const CardSection = (props) => {
-  return <View style={styles.containerStyle}>{props.children}</View>;
+  return (
+    <View style={[styles.containerStyle, props.style]}>{props.children}</View>
+  );
 };
 
 const styles = {
---
+++ manager/src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,13 @@
+import {EMPLOYEE_UPDATE} from '../actions/types';
+
+const INITIAL_STATE = {name: '', phone: '', shift: ''};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload === {prop: 'name', value: 'Jahanzeb'}
+      return {...state, [action.payload.prop]: action.payload.value};
+    default:
+      return state;
+  }
+};
